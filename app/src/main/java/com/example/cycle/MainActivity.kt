f9e7a05e145431531870eacc4470eaccd3962893
Sashaklim <sashaklimovic995@gmail.com>
2025-06-19 20:04:00 +0300

    2. Структура директорий MainActivity

--- app/src/main/java/com/example/cycle/MainActivity.kt
+++ app/src/main/java/com/example/cycle/MainActivity.kt
@@ -1,20 +1,18 @@
 package com.example.cycle
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var inputX: EditText
+    private lateinit var calculateButton: Button
+    private lateinit var resultText: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+
     }
 }
